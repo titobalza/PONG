8029d3eadf6c9f03dc9b96a2add9f5bd07d96bdc
titobalza <titobalzaromero@gmail.com>
2024-01-30 19:16:49 -0400

    juego

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] public float spd = 8f;
+    [SerializeField] private bool isPaddle1;
+    public float ybound = 0.2f;
+    
+    void Update()
+    {
+      float mov;
+
+      if (isPaddle1)
+      {
+        mov = Input.GetAxisRaw("Vertical");
+      }   
+      else {
+        mov = Input.GetAxisRaw("Vertical2");
+      }
+
+      Vector2 paddlepos=transform.position;
+
+      paddlepos.y = Mathf.Clamp(paddlepos.y + mov *spd *Time.deltaTime, -ybound, ybound);
+      transform.position = paddlepos;
+
+
+    }
+}
