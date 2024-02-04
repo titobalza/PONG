3824f178b46c49c3b063d2fec02b652f4996ee03
titobalza <titobalzaromero@gmail.com>
2024-02-04 14:32:54 -0400

    FUNCIONAL COLORES Y DUPLICADO

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -5,54 +5,42 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] private float initialVelocity = 7f;
-    [SerializeField] private float velocityMultiplier = 1.25f;
+    [SerializeField] private float velocityMultiplier = 1.20f;
+    [SerializeField] private int hitCountToDuplicate = 7;
+    private int currentHitCount = 0;
 
-    private Rigidbody2D ballRb;    
-    
+    private Rigidbody2D ballRb;
+    private Camera mainCamera;
+
     void Start()
     {
         ballRb = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
         Launch();
     }
 
-
     private void Launch()
     {
-        float xVelocity = Random.Range(0, 2) == 0 ? -1 : 1;
-        float yVelocity = Random.Range(0, 2) == 0 ? -1 : 1;
-        ballRb.velocity = new Vector2(xVelocity, yVelocity) * initialVelocity;
-    }
-    private float Multiplier()
-    {
-        float velocidad_actual = ballRb.velocity.magnitude;
-        float p = Random.Range(0.5f, 1f);
-        float d = Random.Range(1f, 3f);
-        float v = Random.Range(0.5f, 3f);
-        if (velocidad_actual < 8f)
+        if (ballRb != null)
         {
-            return d;
-        }
-        else if (velocidad_actual > 16f)
-        {
-            return p;
-        }
-        else
-        {
-            return v;
+            float xVelocity = Random.Range(0, 2) == 0 ? -1 : 1;
+            float yVelocity = Random.Range(0, 2) == 0 ? -1 : 1;
+            ballRb.velocity = new Vector2(xVelocity, yVelocity) * initialVelocity;
         }
     }
 
-    private void onCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
         if (collision.gameObject.CompareTag("Paddle"))
         {
-            GameManager.Instance.UpdateSpeed();
-            ballRb.velocity *= velocityMultiplier;         
-        }
-        else if (collision.gameObject.CompareTag("Pared"))
-        {
-            GameManager.Instance.UpdateSpeed();
+            ballRb.velocity *= velocityMultiplier;
+            currentHitCount++;
+            if (currentHitCount >= hitCountToDuplicate)
+            {
+                currentHitCount = 0;
+                DuplicateBall();
+                ChangeBackgroundColor();
+            }
         }
     }
 
@@ -62,14 +50,45 @@ public class Ball : MonoBehaviour
         {
             GameManager.Instance.GolDer();
             GameManager.Instance.Reinicio();
+            DestroyDuplicateBalls();
             Launch();
         }
         else if (collision.gameObject.CompareTag("Goal2"))
         {
             GameManager.Instance.GolIzq();
             GameManager.Instance.Reinicio();
+            DestroyDuplicateBalls();
             Launch();
         }
     }
 
+    private void DuplicateBall()
+    {
+        GameObject newBall = Instantiate(gameObject, transform.position, Quaternion.identity);
+        Ball newBallComponent = newBall.GetComponent<Ball>();
+        newBallComponent.ballRb = newBall.GetComponent<Rigidbody2D>(); // Assign the ballRb reference to the new ball
+        newBallComponent.mainCamera = mainCamera;
+        newBallComponent.Launch();
+    }
+
+    private void ChangeBackgroundColor()
+    {
+        float r = Random.value;
+        float g = Random.value;
+        float b = Random.value;
+        Color randomColor = new Color(r, g, b);
+        mainCamera.backgroundColor = randomColor;
+    }
+
+    private void DestroyDuplicateBalls()
+    {
+        Ball[] duplicateBalls = FindObjectsOfType<Ball>();
+        foreach (Ball duplicateBall in duplicateBalls)
+        {
+            if (duplicateBall != this)
+            {
+                Destroy(duplicateBall.gameObject);
+            }
+        }
+    }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,12 +10,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Transform paddle1Transform;
     [SerializeField] private Transform paddle2Transform;
     [SerializeField] private Transform ballTransform;
-    [SerializeField] private TextMeshProUGUI ballSpeed;
-
 
     private int gamer1score;
     private int gamer2score;
-    private int h = 0;
 
 
 
@@ -44,16 +41,18 @@ public class GameManager : MonoBehaviour
         gamer2scoreText.text = gamer2score.ToString();
     }
 
-    public void UpdateSpeed()
-    {
-        h++;
-        ballSpeed.text = h.ToString();
-    }
+  public void Reinicio()
+{
+    paddle1Transform.position = new Vector2(paddle1Transform.position.x, 0);
+    paddle2Transform.position = new Vector2(paddle2Transform.position.x, 0);
 
-    public void Reinicio()
+    Ball[] balls = FindObjectsOfType<Ball>();
+    foreach (Ball ball in balls)
     {
-        paddle1Transform.position = new Vector2(paddle1Transform.position.x, 0);
-        paddle2Transform.position = new Vector2(paddle2Transform.position.x, 0);
-        ballTransform.position = new Vector2(0, 0);
+        ball.transform.position = new Vector2(0, 0);
+        Rigidbody2D ballRb = ball.GetComponent<Rigidbody2D>();
+        if (ballRb != null)
+            ballRb.velocity = Vector2.zero;
     }
+}
 }
